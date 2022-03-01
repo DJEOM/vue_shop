60591f139d46d11367a86e7174a4b2356226b380
Demon <958929648@qq.com>
2022-03-01 23:21:57 +0800

    111

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,14 @@
 <template>
-    <div class="login_container">
-        登录界面
-    </div>
+  <div class="login_container">登录界面</div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
 .login_container {
-    background-color: #2b4b6b;
-    height: 100%;
+  background-color: #2b4b6b;
+  height: 100%;
 }
 </style>
