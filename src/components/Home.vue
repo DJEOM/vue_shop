1b540122568c593d82cd76b3e5f5560e5b708649
Demon <958929648@qq.com>
2022-03-02 21:49:48 +0800

    3.2日的更改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,10 +14,10 @@
         <!-- 页面主体 -->
         <el-container>
             <!-- 侧边栏 -->
-            <el-aside width="200px">
+            <el-aside width="isCollapse ? '64px' : '200px' ">
                 <div class="toggle-button" @click="toggleCollapse">|||</div>
-            <!-- 侧边菜单栏 -->
-                <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eef" unique-opened :collapse="isCollapse" collapse-transition>
+                <!-- 侧边菜单栏 -->
+                <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eef" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
                     <!-- 一级菜单 -->
                     <el-submenu :index="value.id + ''" v-for="value in menulist" :key="value.id">
                         <!-- 一级菜单模板 -->
@@ -29,7 +29,7 @@
                         </template>
 
                             <!-- 二级菜单 -->
-                        <el-menu-item :index="subItem.id + '' " v-for="subItem in value.children" :key="subItem.id">
+                        <el-menu-item :index=" '/' + subItem.path" v-for="subItem in value.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
                                <!-- 图标 -->
                             <i class="el-icon-menu"></i>
                             <!-- 文本 -->
@@ -39,10 +39,12 @@
                 </el-menu>
             </el-aside>
 
-
-                        <!-- 内容主体 -->
-                        <el-main>Main</el-main>
-                    </el-container>
+            <!-- 内容主体 -->
+            <el-main>
+                <!-- 路由占位符 -->
+                <router-view/>
+            </el-main>
+        </el-container>
     </el-container>
 </template>
 
@@ -61,11 +63,14 @@
                     '145': 'iconfont icon-baobiao',
                 },
                 // 不折叠
-                isCollapse: false
+                isCollapse: false,
+                // 被激活的链接地址
+                activePath: ''
             }
         },
         created () {
             this.getMenuList();
+            this.activePath = window.sessionStorage.getItem('activePath')
         },
         methods: {
             // 退出按钮的点击事件
@@ -86,7 +91,13 @@
             // 点击按钮，切换菜单的折叠与展开
             toggleCollapse(){
                 this.isCollapse = !this.isCollapse
+            },
+            // 保存链接的激活状态
+            saveNavState(activePath){
+                window.sessionStorage.setItem('activePath', activePath)
+                this.activePath = activePath
             }
+
 
             
         },
