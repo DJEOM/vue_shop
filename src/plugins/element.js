f65a3b0a57e85994ec359daa838366bbcdd5b0ac
Demon <958929648@qq.com>
2022-03-07 17:56:36 +0800

    下午的提交

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -8,39 +8,41 @@
  */
 import Vue from "vue";
 import {
-  Button,
-  Form,
-  FormItem,
-  Input,
-  Message,
-  Container,
-  Header,
-  Aside,
-  Main,
-  Menu,
-  Submenu,
-  MenuItemGroup,
-  MenuItem,
-  Breadcrumb,
-  BreadcrumbItem,
-  Card,
-  Row,
-  Col,
-  Table,
-  TableColumn,
-  Switch,
-  Tooltip,
-  Pagination,
-  Dialog,
-  MessageBox,
-  Tag,
-  Tree,
-  Select,
-  Option,
-  Cascader,
-  Alert,
-  Tabs,
-  TabPane
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItemGroup,
+    MenuItem,
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,
+    Row,
+    Col,
+    Table,
+    TableColumn,
+    Switch,
+    Tooltip,
+    Pagination,
+    Dialog,
+    MessageBox,
+    Tag,
+    Tree,
+    Select,
+    Option,
+    Cascader,
+    Alert,
+    Tabs,
+    TabPane,
+    Steps,
+    Step,
 } from "element-ui";
 Vue.use(Button);
 Vue.use(Form);
@@ -73,6 +75,8 @@ Vue.use(Cascader);
 Vue.use(Alert);
 Vue.use(Tabs);
 Vue.use(TabPane);
+Vue.use(Steps);
+Vue.use(Step);
 
 Vue.prototype.$message = Message;
 Vue.prototype.$confirm = MessageBox.confirm;
